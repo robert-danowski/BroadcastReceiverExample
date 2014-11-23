c6408cbc1f86bb9431c3bd7733769509b80f16c8
Robert Danowski <trevny@poczta.onet.pl>
2014-11-23 10:37:37 +0100

    initital

---
+++ app/src/main/java/com/example/teodor/broadcastreceiverexample/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.teodor.broadcastreceiverexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+
+        List<String> strings = new ArrayList<String>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, strings);
+        listView.setAdapter(adapter);
+
+
+    }
+
+}
